883a8252cfdd7a6380c6134fba2036e21c4721f3
mikeunmc <mike.munns@unmc.com>
2015-03-23 15:30:53 -0500

    lesson 3.10

--- app/src/main/java/edu/unmc/sunshine/app/ForecastFragment.java
+++ app/src/main/java/edu/unmc/sunshine/app/ForecastFragment.java
@@ -1,9 +1,11 @@
 package edu.unmc.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
@@ -63,35 +65,51 @@ public class ForecastFragment extends Fragment {
         int id = item.getItemId();
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_refresh) {
-            //Log.v(ForecastFragment.class.getSimpleName(), "Forecast: Refresh ");
-            FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("68112");
+            updateWeather();
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void updateWeather() {
+        FetchWeatherTask weatherTask = new FetchWeatherTask();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String location = prefs.getString(getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default));
+        //weatherTask.execute("68112");
+        weatherTask.execute(location);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateWeather();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        String [] forecastArray = {
-            "Today - Sunny - 69/33",
-            "Tomorrow - Clear - 70/36",
-            "Friday - Sunny - 65/34",
-            "Saturday - Sunny - 68/38",
-            "Sunday - Sunny - 72/30",
-            "Monday - Cloudy - 50/31",
-            "Tuesday - Cloudy - 54/32"
-        };
+        //String [] forecastArray = {
+        //    "Today - Sunny - 69/33",
+        //    "Tomorrow - Clear - 70/36",
+        //    "Friday - Sunny - 65/34",
+        //    "Saturday - Sunny - 68/38",
+        //    "Sunday - Sunny - 72/30",
+        //    "Monday - Cloudy - 50/31",
+        //    "Tuesday - Cloudy - 54/32"
+        //};
 
-        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+        //Log.v("Michael Munns", "Location is " + R.string.pref_location_default );
+
+        //List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
 
         mForecastAdapter = new ArrayAdapter<String>(
                 getActivity(),                     // current context
                 R.layout.list_item_forecast,       // ID of the list item layout xml file
                 R.id.list_item_forecast_textview,  // ID oif the textview to populate
-                weekForecast);                     // Forecast data
+                new ArrayList<String>());
+                //weekForecast);                     // Forecast data
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
